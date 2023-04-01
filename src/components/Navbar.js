56619e0b966ee47e2001157f2de65eda28888d50
Ronald Abu Saleh <lancerrnld@gmail.com>
2023-04-01 15:06:13 -0400

    Fix avatar image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import logo from './../assets/logo.png';
+import avatar from './../assets/avatar.png';
 
 const Navbar = () => {
   const [show, handleShow] = useState(false);
@@ -21,7 +22,7 @@ const Navbar = () => {
       <img className="navbar__logo" src={logo} alt="Netflix Logo" />
       <img
         className="navbar__avatar"
-        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        src={avatar}
         alt="Netflix Avatar"
       />
     </div>
